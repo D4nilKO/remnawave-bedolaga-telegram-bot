3764b96bf278741a4174107079749e8c47619a54
Dxnil <ponkratenko.danil@mail.ru>
2026-02-26 17:33:00 +0300

    fix(migrations): resolve alembic multiple heads
    - Renumber button_click_logs FK repair migration from 0010 to 0012
    - Chain it after 0011 RBAC migration to keep a single Alembic head

--- migrations/alembic/versions/0012_fix_button_click_logs_user_fk.py
+++ migrations/alembic/versions/0012_fix_button_click_logs_user_fk.py
@@ -1,7 +1,7 @@
 """fix button_click_logs.user_id foreign key to users.id
 
-Revision ID: 0010
-Revises: 0009
+Revision ID: 0012
+Revises: 0011
 Create Date: 2026-02-18
 
 Normalizes legacy databases where button_click_logs.user_id could be tied to
@@ -16,8 +16,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '0010'
-down_revision: Union[str, None] = '0009'
+revision: str = '0012'
+down_revision: Union[str, None] = '0011'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
